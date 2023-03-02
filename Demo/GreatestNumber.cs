e130e9c83c386ee74b50e84f7c8995c3fb8a5e0d
Anilrocky <anilkumarac96@gmail.com>
2023-03-02 19:35:20 +0530

    [Anil] Add:Files to do Addition/GreatestNumber/PrimeNumber

---
+++ Demo/GreatestNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class GreatestNumber
+    {
+        static int a = 20; static int b = 10;
+        static void Main(string[] args)
+        {
+            if (a > b)
+                {
+                Console.WriteLine("a is greater than b");
+                }
+            else
+            {
+                Console.WriteLine("b is greater than a");
+            }
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,18 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Anil");
 Console.WriteLine("Welcome to new branch");
-
-//Adding file for Arithmetic operation
-namespace Demo
-{
-    class Program
-    {
-        static int a = 20;
-        static int b = 10;
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Addition is: " + (a+b));
-        }
-    }
-}
