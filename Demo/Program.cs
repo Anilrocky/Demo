bc628c118815b3e8a6cf6b0dba9b672f7e348e41
Anilrocky <anilkumarac96@gmail.com>
2023-03-02 09:28:50 +0530

    [Anil] Delete:Extra lines

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,17 +2,3 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Anil");
 Console.WriteLine("Welcome to new branch");
-
-//Adding file for Arithmetic operation
-namespace Demo
-{
-    class Program
-    {
-        static int a = 20;
-        static int b = 10;
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Addition is: " + (a+b));
-        }
-    }
-}
