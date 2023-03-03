109bf489002466d026ffa651107d5e57c5f953e0
Anilrocky <anilkumarac96@gmail.com>
2023-03-03 20:33:44 +0530

    [Anil] Add:Finding Factorial of Number

---
+++ Project/Factorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Project
+{
+    public class Factorial
+    {
+        public void FactorialOfNumber()
+        {
+            Console.WriteLine("Enter any Number");
+            int num = int.Parse(Console.ReadLine());
+            int fact = 1;
+            for (int i = 1; i <=num; i++)
+            {
+                fact = fact* i;
+            }
+            Console.WriteLine($"Factorial of {num} is: {fact}");
+            Console.ReadLine();
+        }
+    }
+}
