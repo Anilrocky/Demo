77a2cc22799d7a9b731274794d97c040a669c4a6
Anilrocky <anilkumarac96@gmail.com>
2023-03-03 19:44:31 +0530

    [Anil] Add:Folder Structure Created

---
+++ Project/EvenOdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Project
+{
+    public class EvenOdd
+    {
+        public void EvenOrOdd()
+        {
+            Console.WriteLine("Enter any Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + " is Even");
+            }
+            else
+            {
+                Console.WriteLine(num + " is Odd");
+            }
+        }
+    }
+}
