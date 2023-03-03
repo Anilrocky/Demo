017c21bc04c54b96052b8a22afeaa2564d336b5a
Anilrocky <anilkumarac96@gmail.com>
2023-03-03 19:34:28 +0530

    [Anil] Add:Folder Structure Created

---
+++ Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenOdd evenOdd = new EvenOdd();
+            evenOdd.EvenOrOdd();
+        }
+    }
+}
